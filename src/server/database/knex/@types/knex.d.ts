9f9aedb2b366510c3b9155fa84cb2bfb9349da1e
Ewerton Lima <ewerton_rlima@hotmail.com>
2023-06-26 11:30:37 -0300

    feat: config migrations, Knex and ICidade

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,8 +1,8 @@
-
+import { ICidade } from "../../models"
 
 declare module 'knex/types/tables' {
   interface Tables{
-    //cidades: ICidade
+    cidade: ICidade
     //pessoa: IPessoa
     //Susurario: IUsuario
   }
--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 
 export const development: Knex.Config = {
-  client: 'sqlit3',
+  client: 'sqlite3',
   useNullAsDefault: true,
   connection: {
     filename: path.resolve(__dirname,'..','..','..','..','database.sqlite')
